2a619f8f7406de86b8ed21a5b474361133724dbd
reddevilcero <reddevil_cero@hotmail.com>
2020-07-21 14:17:57 +0100

    create 4 boxed styled dynamically

--- App.js
+++ App.js
@@ -1,32 +1,25 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  SafeAreaView,
-  Button,
-} from 'react-native';
-import styled from 'styled-components/native';
+import { StyleSheet, Text, StatusBar, SafeAreaView } from 'react-native';
+import StyledBox from './components/StyledBox';
+import { activateKeepAwake } from 'expo-keep-awake';
 
-const LikeButton = styled.View`
-  padding: 10px;
-  border-radius: 5px;
-  background-color: red;
-`;
-const styledButton = styled.Button`
-  background-color: red;
-`;
+activateKeepAwake();
 
+// import styled from 'styled-components/native';
+
+const colors = {
+  Cyan: '#2aa198',
+  Blue: '#268bd2',
+  Magenta: '#d33682',
+  Orange: '#cb4b16',
+};
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.myColor]}>
-        <LikeButton style={styles.button}>
-          <Text>Hello From App</Text>
-        </LikeButton>
-        <styledButton title="click me" style={styles.button} />
-      </View>
+      <Text style={styles.text}>Here are some boxes of different colors</Text>
+      {Object.keys(colors).map((color) => {
+        return <StyledBox key={color} color={color} value={colors[color]} />;
+      })}
     </SafeAreaView>
   );
 };
@@ -34,20 +27,15 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  button: {
-    elevation: 2,
-    margin: 10,
-  },
-  myColor: {
-    backgroundColor: 'aquamarine',
-  },
-  container: {
-    marginTop: StatusBar.currentHeight,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
   safeArea: {
+    marginTop: StatusBar.currentHeight,
     flex: 1,
+    padding: 10,
+    alignItems: 'stretch',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
