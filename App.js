55439bd0b9dbea0a8c2aa99c972afc27c64e5b89
reddevilcero <reddevil_cero@hotmail.com>
2020-07-21 12:41:14 +0100

    palying with advance styles.

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  Button,
+} from 'react-native';
+import styled from 'styled-components/native';
+
+const LikeButton = styled.View`
+  padding: 10px;
+  border-radius: 5px;
+  background-color: red;
+`;
+const styledButton = styled.Button`
+  background-color: red;
+`;
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello From App</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.myColor]}>
+        <LikeButton style={styles.button}>
+          <Text>Hello From App</Text>
+        </LikeButton>
+        <styledButton title="click me" style={styles.button} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  button: {
+    elevation: 2,
+    margin: 10,
+  },
+  myColor: {
     backgroundColor: 'aquamarine',
+  },
+  container: {
     marginTop: StatusBar.currentHeight,
-    paddingHorizontal: 30,
-    paddingVertical: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
   },
 });
