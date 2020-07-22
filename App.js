28f4e783f8157485d76dce137ca2db3420c6f667
reddevilcero <reddevil_cero@hotmail.com>
2020-07-22 22:30:56 +0100

    using proper Flatlist instead of map like in React

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, StatusBar, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  StatusBar,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
 import StyledBox from './components/StyledBox';
 import { activateKeepAwake } from 'expo-keep-awake';
+
 
 activateKeepAwake();
 
@@ -13,10 +20,38 @@ const colors = {
   Blue: '#268bd2',
   Magenta: '#d33682',
 };
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
+      <FlatList
+        data={COLORS}
+        keyExtractor={(item) => item.hexCode}
+        ListHeaderComponent={() => <Text style={styles.text}>Solarize</Text>}
+        renderItem={({ item }) => (
+          <StyledBox colorName={item.colorName} hexCode={item.hexCode} />
+        )}
+      />
+
       <Text style={styles.text}>Here are some boxes of different colors</Text>
+      {/* DO NOT USE MAP ON REACT NATIVE */}
       {Object.keys(colors).map((color) => {
         return (
           <StyledBox key={color} colorName={color} hexCode={colors[color]} />
--- components/StyledBox.js
+++ components/StyledBox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 const StyledBox = ({ hexCode, colorName }) => {
   const styles = StyleSheet.create({
     container: {
@@ -10,7 +11,11 @@ const StyledBox = ({ hexCode, colorName }) => {
       alignItems: 'center',
     },
     text: {
-      color: '#fff',
+      color:
+        // return false if color close to the 10% of the white spectre
+        parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.15
+          ? '#000'
+          : '#fff',
       fontSize: 18,
       fontWeight: 'bold',
     },
