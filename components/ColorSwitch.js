b8e29bbff28ab6c928256f83928ef42d49e7e3b8
lorrydriveloper <lorrydriveloper@gmail.com>
2020-07-24 18:50:39 +0100

    better style and mini preview of color

--- components/ColorSwitch.js
+++ components/ColorSwitch.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Switch } from 'react-native-gesture-handler';
+import { shadow } from '../sharedStyles/shadow';
 
 const ColorSwitch = ({ item, addColor, removeColor }) => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -17,8 +18,11 @@ const ColorSwitch = ({ item, addColor, removeColor }) => {
   }, [isEnabled]);
 
   return (
-    <View>
-      <Text>{item.colorName}</Text>
+    <View style={styles.container}>
+      <View style={styles.colorContainer}>
+        <View style={[styles.minicolor, { backgroundColor: item.hexCode }]} />
+        <Text>{item.colorName}</Text>
+      </View>
       <Switch onValueChange={toggleSwitch} value={isEnabled} />
     </View>
   );
@@ -26,4 +30,25 @@ const ColorSwitch = ({ item, addColor, removeColor }) => {
 
 export default ColorSwitch;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  minicolor: {
+    ...shadow,
+    height: 25,
+    width: 25,
+    borderRadius: 5,
+    marginEnd: 5,
+  },
+  colorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+    marginVertical: 5,
+    borderBottomColor: 'lavender',
+    borderBottomWidth: 2,
+    padding: 5,
+  },
+});
